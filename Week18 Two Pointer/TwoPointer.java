8640f89de2ad1873cf1bdd281cfa0dc9d9370f3e
Bektur13 <duyshembekov04@mail.ru>
2025-07-29 12:33:30 -0700

    Completed Two Pointer topic in GH

--- Week18 Two Pointer/TwoPointer.java
+++ Week18 Two Pointer/TwoPointer.java
@@ -1,5 +1,37 @@
 class TwoPointer {
     public static void main(String[] args) {
+        // Two Sum || Input is Sorted
+        public int[] twoSum(int[] numbers, int target) {
+            int left = 0;
+            int right = numbers.length - 1;
+            while(left <= right) {
+                if(numbers[left] + numbers[right] == target) {
+                    left++;
+                    right++;
+                    return new int[] {left, right};
+                }
+                if(numbers[left] + numbers[right] > target) {
+                    right--;
+                }else {
+                    left++;
+                }
+            }
+            return new int[] {};
+        }
+        // Move Zeros
+        public void moveZeroes(int[] nums) {
+            int j = 0;
+            for(int i = 0; i < nums.length; i++) {
+                if(nums[i] != 0) {
+                    if(i != j) {
+                        int temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                    j++;
+                }
+            }
+        }
         // String Compression
         public int compress(char[] chars) {
             int n = chars.length;
@@ -28,6 +60,28 @@ class TwoPointer {
                 streakStart = streakEnd + 1;
             }
             return indexAnswer;
+        }
+        // Container With Most Water
+        public int maxArea(int[] height) {
+            int left = 0;
+            int right = height.length - 1;
+            int answer = 0;
+
+            while(left < right) {
+                int width = right - left;
+                int h = Math.min(height[left], height[right]);
+                int volume = width * h;
+
+                answer = Math.max(answer, volume);
+
+                if(height[left] < height[right]) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+
+            return answer;
         }
     }
 }
