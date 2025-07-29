474d801b40adcce5545a8978961497a6f41035bf
Bektur13 <duyshembekov04@mail.ru>
2025-07-29 12:27:06 -0700

    Solved Binary Search problem

--- Week17 Binary Search/BinarySearch.java
+++ Week17 Binary Search/BinarySearch.java
@@ -1,6 +1,32 @@
-public static void main(String[] args) {
-//  4. Capacity to ship n packages within d days
-    class Solution {
+class Solution {
+    public static void main(String[] args) {
+        // Kth Smallest Number in Multiplication Table
+        public int findKthNumber(int m, int n, int k) {
+            int left = 1;
+            int right = m * n;
+            int answer = -1;
+            while (left <= right) {
+                int middle = left + (right - left) / 2;
+                int count = countNumsLessThanOrEqualToValueInMultTableMbyN(middle, m, n);
+                if (count >= k) {
+                    answer = middle;
+                    right = middle - 1;
+                } else {
+                    left = middle + 1;
+                }
+            }
+            // T: O(log(m*n) * m) <-> O(m*n)
+            return answer;
+        }
+
+        private int countNumsLessThanOrEqualToValueInMultTableMbyN(int value, int m, int n) {
+            int count = 0;
+            for (int i = 1; i <= m; i++) {
+                count += Math.min(value / i, n);
+            }
+            return count;
+        }
+        // Capacity to ship n packages within d days
         public int shipWithinDays(int[] weights, int days) {
             int smallestCapacity = weights[0];
             int sumOfWeights = 0;
