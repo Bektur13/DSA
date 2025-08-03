84ab6063cd8860aa50b34ee4c734bef498253796
Bektur13 <duyshembekov04@mail.ru>
2025-08-02 20:20:49 -0700

    Solve 2 problems on Queue

---
+++ Week23/Queue.java
@@ -0,0 +1,112 @@
+public class Queue {
+    public static void main(String[] args) {
+        // IMPLEMENT QUEUE USING STACKS
+        class MyQueue {
+            Stack<Integer> input = new Stack();
+            Stack<Integer> output = new Stack();
+
+            public void push(int x) {
+                input.push(x);
+            }
+
+            public int pop() {
+                int x = peek();
+                output.pop();
+                return x;
+            }
+
+            public int peek() {
+                if(output.empty()) {
+                    while(!input.empty()) {
+                        output.push(input.pop());
+                    }
+                }
+                return output.peek();
+            }
+
+            public boolean empty() {
+                return input.empty() && output.empty();
+            }
+        }
+
+        // DESIGN CIRCULAR QUEUE
+        class MyCircularDeque {
+            private int[] res;
+            private int front, size, capacity;
+
+            public MyCircularDeque(int k) {
+                res = new int[k];
+                front = 0;
+                size = 0;
+                capacity = k;
+            }
+
+            public boolean insertFront(int value) {
+                if(size == capacity) {
+                    return false;
+                }else {
+                    front = (front - 1 + capacity) % capacity;
+                    res[front] = value;
+                    size++;
+                    return true;
+                }
+            }
+
+            public boolean insertLast(int value) {
+                if(size == capacity) {
+                    return false;
+                }else {
+                    int rear = (front + size) % capacity;
+                    res[rear] = value;
+                    size++;
+                    return true;
+                }
+            }
+
+            public boolean deleteFront() {
+                if(size == 0) {
+                    return false;
+                }else {
+                    front = (front + 1) % capacity;
+                    size--;
+                    return true;
+                }
+            }
+
+            public boolean deleteLast() {
+                if(size == 0) {
+                    return false;
+                }else {
+                    int rear = (front + size - 1) % capacity;
+                    size--;
+                    return true;
+                }
+            }
+
+            public int getFront() {
+                if(isEmpty()) {
+                    return -1;
+                }else {
+                    return res[front];
+                }
+            }
+
+            public int getRear() {
+                if(isEmpty()) {
+                    return -1;
+                }else {
+                    int rear = (front + size - 1) % capacity;
+                    return res[rear];
+                }
+            }
+
+            public boolean isEmpty() {
+                return size == 0;
+            }
+
+            public boolean isFull() {
+                return size >= capacity;
+            }
+        }
+    }
+}
