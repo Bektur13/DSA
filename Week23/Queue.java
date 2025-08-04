1fbf2e8f732f2ee322e2df33d527b6d32db159f7
Bektur13 <duyshembekov04@mail.ru>
2025-08-04 14:20:41 -0700

    Solve problem on Queue/Deque

--- Week23/Queue.java
+++ Week23/Queue.java
@@ -137,5 +137,35 @@ public class Queue {
 
             return res;
         }
+
+        // LONGEST CONTINIOUS SUBARRAY WITH ABSOLUTE DIFF LESS THAN OR EQUAL TO LIMIT
+        public int longestSubarray(int[] nums, int limit) {
+            Deque<Integer> maxDeque = new LinkedList<>();
+            Deque<Integer> minDeque = new LinkedList<>();
+
+            int left = 0;
+            int maxLength = 0;
+
+            for(int right = 0; right < nums.length; ++right) {
+                while(!maxDeque.isEmpty() && maxDeque.peekLast() < nums[right]) maxDeque.pollLast();
+                while(!minDeque.isEmpty() && minDeque.peekLast() > nums[right]) minDeque.pollLast();
+                maxDeque.offerLast(nums[right]);
+                minDeque.offerLast(nums[right]);
+
+                while(maxDeque.peekFirst() - minDeque.peekFirst() > limit) {
+                    if(maxDeque.peekFirst() == nums[left]) {
+                        maxDeque.pollFirst();
+                    }
+                    if(minDeque.peekFirst() == nums[left]) {
+                        minDeque.pollFirst();
+                    }
+                    ++left;
+                }
+
+                maxLength = Math.max(maxLength, right - left + 1);
+            }
+
+            return maxLength;
+        }
     }
 }
