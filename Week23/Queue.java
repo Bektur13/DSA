74987dea07c17e64eb05092c42b7a1ad7fb693ba
Bektur13 <duyshembekov04@mail.ru>
2025-08-04 13:32:47 -0700

    Solve 1 problem using Deque

--- Week23/Queue.java
+++ Week23/Queue.java
@@ -108,5 +108,34 @@ public class Queue {
                 return size >= capacity;
             }
         }
+
+        // SLIDING WINDOW MAXIMUM
+        public int[] maxSlidingWindow(int[] nums, int k) {
+            if(nums == null || k <= 0) {
+                return new int[0];
+            }
+
+            int n = nums.length;
+            int[] res = new int[n - k + 1];
+            int ri = 0;
+
+            Deque<Integer> d = new ArrayDeque<>();
+            for(int i = 0; i < nums.length; i++) {
+                while(!d.isEmpty() && d.peekFirst() < i - k + 1) {
+                    d.pollFirst();
+                }
+
+                while(!d.isEmpty() && nums[i] > nums[d.peekLast()]) {
+                    d.pollLast();
+                }
+
+                d.offer(i);
+                if(i >= k - 1) {
+                    res[ri++] = nums[d.peekFirst()];
+                }
+            }
+
+            return res;
+        }
     }
 }
