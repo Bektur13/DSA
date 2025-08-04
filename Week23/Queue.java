acd33ba3c6ffaf3186ed6897c966ac5ca44d4acd
Bektur13 <duyshembekov04@mail.ru>
2025-08-04 14:23:24 -0700

    Completed W23

--- Week23/Queue.java
+++ Week23/Queue.java
@@ -109,6 +109,29 @@ public class Queue {
             }
         }
 
+        // SHORTEST SUBARRAY WITH SUM AT LEAST K
+        public int shortestSubarray(int[] nums, int k) {
+            int N = nums.length;
+            int res = N + 1;
+
+            long[] B = new long[N + 1];
+            for(int i = 0; i < N; i++) {
+                B[i + 1] = B[i] + nums[i];
+            }
+
+            Deque<Integer> d = new ArrayDeque<>();
+            for(int i = 0; i < N + 1; i++) {
+                while(d.size() > 0 && B[i] - B[d.getFirst()] >= k) {
+                    res = Math.min(res, i - d.pollFirst());
+                }
+                while(d.size() > 0 && B[i] <= B[d.getLast()]) {
+                    d.pollLast();
+                }
+                d.addLast(i);
+            }
+            return res <= N ? res : -1;
+        }
+
         // SLIDING WINDOW MAXIMUM
         public int[] maxSlidingWindow(int[] nums, int k) {
             if(nums == null || k <= 0) {
