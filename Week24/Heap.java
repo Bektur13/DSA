1fdbc50c1cc290fbbc6c1e8a5e529fef8dc55c83
Bektur13 <duyshembekov04@mail.ru>
2025-08-26 12:48:45 -0700

    Solve 1 more on Heap/PriorityQueue

--- Week24/Heap.java
+++ Week24/Heap.java
@@ -370,5 +370,29 @@ public class Heap {
                 return (minHeap.peek() + maxHeap.peek()) / 2.0d;
             }
         }
+
+        // K CLOSEST POINTS TO ORIGIN
+        public int[][] kClosest(int[][] points, int k) {
+            PriorityQueue<int[]> maxHeap = new PriorityQueue<>(new Comparator<int[]>() {
+                @Override
+                public int compare(int[] p1, int[] p2) {
+                    return (p2[0] * p2[0] + p2[1] * p2[1]) - (p1[0] * p1[0] + p1[1] * p1[1]);
+                }
+            });
+
+            for(int[] point : points) {
+                maxHeap.offer(point);
+                if(maxHeap.size() > k) {
+                    maxHeap.poll();
+                }
+            }
+
+            int[][] result = new int[k][2];
+            for(int i = 0; i < k; i++) {
+                result[i] = maxHeap.poll();
+            }
+
+            return result;
+        }
     }
 }
