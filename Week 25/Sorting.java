5c3549252f9fdbb8787c700a0d8dc92186a1e99e
Bektur13 <duyshembekov04@mail.ru>
2025-09-10 15:39:13 -0700

    Optimize solution

--- Week 25/Sorting.java
+++ Week 25/Sorting.java
@@ -99,16 +99,34 @@ public class Sorting {
         }
             // AZRET BAIKE'S SOLUTION OPTIMIZED
         public int[] sortArrayOptimized(int[] nums) {
-            PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-            for(int num : nums) {
-                minHeap.offer(num);
+            int n = nums.length;
+
+            for(int i = 0; i < n; i++) {
+                nums[i] += 50000;
             }
 
-            for(int i = 0; i < nums.length; i++) {
-                nums[i] = minHeap.poll();
+            int k = 100000;
+            int[] b = new int[n];
+            int[] c = new int[k + 1];
+            for(int i = 0; i < k; i++) {
+                c[i] = 0;
+            }
+            for(int j = 0; j < n; j++) {
+                c[nums[j]]++;
+            }
+            for(int j = 1; j <= k; j++) {
+                c[j] += c[j-1];
+            }
+            for(int j = n - 1; j >= 0; j--) {
+                b[c[nums[j]] - 1] = nums[j];
+                c[nums[j]]--;
             }
 
-            return nums;
+            for(int i = 0; i < n; i++){
+                b[i] -= 50000;
+            }
+
+            return b;
         }
     }
 }
